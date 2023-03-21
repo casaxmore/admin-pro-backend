49aa66750ab6907679481529e844a5b4981c10dc
casaxmore <casaxmore@gmail.com>
2023-03-21 17:33:39 +0100

    JWT final

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,17 +5,19 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
-
 const {
   getUsuarios,
   crearUsuarios,
   actualizarUsuario,
   borrarUsuario
 } = require("../controllers/usuarios");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-router.get("/", getUsuarios);
+
+
+router.get("/", validarJWT , getUsuarios);
 
 router.post(
   "/",
@@ -31,6 +33,7 @@ router.post(
 
 router.put("/:id", 
  [
+    validarJWT,
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),   
     check("role", "El role es obligatorio").not().isEmpty(),
@@ -40,6 +43,7 @@ router.put("/:id",
 
  router.delete("/:id", 
  [
+    validarJWT,
     validarCampos,
  ]
  , borrarUsuario);
