813eff54edabab2fad6867e4cf168daf3c2bad17
casaxmore <casaxmore@gmail.com>
2023-03-29 16:06:40 +0200

    renew con usuario con el uid

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,9 +93,13 @@ const renewToken = async(req, res = response) => {
   // Generar el TOKEN - JWT
   const token = await generarJWT(uid);
 
+  // Obtener el usuario
+  const usuario = await Usuario.findById(uid);
+
   res.json({
     ok: true,
-    token
+    token,
+    usuario
   });
 
 }
